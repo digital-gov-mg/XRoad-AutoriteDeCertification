8a938bf455abd9bad3e38095d482d7332bb84d15
EGM <contact@digital.gov.mg>
2020-10-02 17:02:33 +0300

    Init push

---
+++ installation.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+    timedatectl set-timezone Africa/Nairobi;
+    hostnamectl set-hostname ejbca; 
+    sudo cp -rf configurer-jboss/ /opt/
+    apt-get update ;
+    apt-get -y install  openjdk-8-jre-headless ca-certificates-java;
+    apt-get -y install openjdk-8-jdk unzip ntp ant ant-optional postgresql postgresql-client;    
+    mkdir -p  /etc/jboss; cd /opt/;
+    unzip -q /opt/ejbca_ce_6_10_1_2.zip;
+    unzip -q /opt/jboss-eap-7.0.0.zip;
+    mv /opt/ejbca_ce_6_10_1_2 /opt/ejbca;
+    mv /opt/jboss-eap-7.0 /opt/jboss;
+    mv /opt/configurer-jboss /opt/scripts; 
+    rm -f /opt/ejbca/modules/cesecore-p11/src/sun/security/pkcs11/CESeCoreUtils.java
+    cp /opt/scripts/CESeCoreUtils.java /opt/ejbca/modules/cesecore-p11/src/sun/security/pkcs11/
+    cp /opt/scripts/ejbca/* /opt/ejbca/conf/;
+    cp /opt/scripts/jboss.conf /etc/jboss/;
+    cp /opt/scripts/jboss.service /etc/systemd/system/;
+    cp /opt/scripts/launch.sh /opt/jboss/bin/;
+    chmod +x /opt/jboss/bin/launch.sh;
+
+    groupadd -r jboss;
+    useradd -r -g jboss -d /opt/jboss -s /sbin/nologin jboss;
+    chown -R jboss:jboss /opt/jboss;
+    chown -R jboss:jboss /opt/ejbca;
+    sudo chmod -R 775 /opt/ejbca;
+    systemctl start jboss.service;
+    systemctl enable jboss.service;
+    service jboss start;
+#EJBCA
+sudo -u postgres psql -U postgres <<OMG
+ CREATE USER ejbca WITH PASSWORD 'ejbca';
+ CREATE DATABASE ejbca WITH OWNER ejbca ENCODING 'UTF8' ;
+OMG
